4074fcf7f75a81b08bdbcc18cdb91a6602e0557c
Joshua Freeman <70025198+joshuafreemn@users.noreply.github.com>
2021-02-13 21:06:29 +0000

    + .title() to sopping loop.

--- toppings.py
+++ toppings.py
@@ -1,6 +1,6 @@
 requested_toppings = ['mushrooms', 'pineapple', 'green peppers', 'extra pineapple']
 
 for topping in requested_toppings:
-	print("Adding " + topping + ".")
+	print("Adding " + topping.title() + ".")
 
 print("\nYour pizza is finsihed.")
