ec2b10dd9e6420610f7bf936886445653128f1cd
Joshua Freeman <70025198+joshuafreemn@users.noreply.github.com>
2021-02-13 23:15:58 +0000

    +for loop +admin message

--- hello_admin.py
+++ hello_admin.py
@@ -1,1 +1,7 @@
 usernames = ['harry', 'eliott', 'roberto', 'katy', 'amiee', 'sarah', 'admin']
+
+for user in usernames:
+	if 'admin' in user:
+		print("Hello Admin, would you like to see a status report?")
+	else:
+		print("Welcome back "+ user.title() +", thankyou for logging in again.")
