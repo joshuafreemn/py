65dddac97fe06e5a85bfbdbd3969bf370d20d82d
Joshua Freeman <70025198+joshuafreemn@users.noreply.github.com>
2021-02-13 21:35:35 +0000

    adding error message
    adding an error message if the pizzaria doesn't have the request pizza topping.

--- toppings.py
+++ toppings.py
@@ -1,10 +1,14 @@
 available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra pineapple', 'jalapeno']
 
-requested_toppings = ['mushrooms', 'pineapple', 'jalapeno', 'extra pineapple']
+requested_toppings = ['mushrooms', 'anchiovies', 'pineapple', 'extra pineapple']
 
 if requested_toppings:
 	for topping in requested_toppings:
-		print("Adding " + topping.title() + ".")
-	print("\nYour pizza is finito.")
+		if topping in available_toppings:
+			print("Adding " + topping.title() + ".")
+		else:
+			print("Sorry Bud, all out of " + topping + ".")
+	print("\nYou're pizza is finito!")
 else:
 	print("Don't be a plain jane, add some toppings to ya pizza!")
+
