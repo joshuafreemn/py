29832cce476162ba88c2bb06d86384c7e21c2112
Joshua Freeman <70025198+joshuafreemn@users.noreply.github.com>
2021-02-13 21:09:37 +0000

    Out of peppers
    adding a conditional statment to check if green peppers has been requested. if so the program will infrom user that the reseruant has run out of green peppers.

--- toppings.py
+++ toppings.py
@@ -1,6 +1,9 @@
 requested_toppings = ['mushrooms', 'pineapple', 'green peppers', 'extra pineapple']
 
 for topping in requested_toppings:
-	print("Adding " + topping.title() + ".")
+	if topping == 'green peppers':
+		print("Sorry, We're all out of green peppers right now.")
+	else:
+		print("Adding " + topping.title() + ".")
 
 print("\nYour pizza is finsihed.")
