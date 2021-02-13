2ccf456db52a47cef5fe2b164dee7092b588bc6f
Joshua Freeman <70025198+joshuafreemn@users.noreply.github.com>
2021-02-13 21:05:33 +0000

    adding a for loop
    adding a for loop to add toppings to pizza to automaically add any topping to pizza.

--- toppings.py
+++ toppings.py
@@ -1,14 +1,6 @@
 requested_toppings = ['mushrooms', 'pineapple', 'green peppers', 'extra pineapple']
 
-if 'mushrooms' in requested_toppings:
-	print("Adding mushrooms.")
-if 'pepperoni' in requested_toppings:
-	print("Adding pepperoni")
-if 'extra cheese' in requested_toppings:
-	print("Adding extra cheese.")
-if 'pineapple' in requested_toppings:
-	print("Adding pineapple.")
-if 'extra pineapple' in requested_toppings:
-	print("Adding extra pineapple.")
+for topping in requested_toppings:
+	print("Adding " + topping + ".")
 
 print("\nYour pizza is finsihed.")
