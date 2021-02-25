a37b9297630efc0bad5a1d44b8cf96be1ad6fab9
Joshua Freeman <70025198+joshuafreemn@users.noreply.github.com>
2021-02-25 19:34:34 +0000

    more deictionaries
    adding a single key-value pair, and an expretion that looks up the points in the alien dictionary to print a statment

--- alien.py
+++ alien.py
@@ -2,3 +2,12 @@ alien_0 = {'color': 'green', 'points': 5}
 
 print(alien_0['color'])
 print(alien_0['points'])
+
+#dictionary with one key-value pair
+alien_1 = {'color': 'blue'}
+
+print(alien_1['color'])
+
+#lookup points of alien_0
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points!")
