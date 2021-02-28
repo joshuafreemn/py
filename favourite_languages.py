3b933390d2b11157e1408a82c6f6ce8c6991c7bb
Joshua Freeman <70025198+joshuafreemn@users.noreply.github.com>
2021-02-28 21:45:25 +0000

    dictionary of similar objects

---
+++ favourite_languages.py
@@ -0,0 +1,8 @@
+favorite_languages = {
+	'jen' : 'python',
+	'sarah' : 'c',
+	'edward' : 'ruby',
+	'phil' : 'python',
+}
+
+print("Sarah's fave language is " + favorite_languages['sarah'].title() + ".")
