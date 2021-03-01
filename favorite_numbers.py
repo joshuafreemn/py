e9b0fdb24fde89c66d4445b1dbfba59e82495b9b
Joshua Freeman <70025198+joshuafreemn@users.noreply.github.com>
2021-03-01 20:37:44 +0000

    .py dictionaries

---
+++ favorite_numbers.py
@@ -0,0 +1,9 @@
+fav_num = {
+	'jill' : 4,
+	'jon' : 12,
+	'jack' : 28,
+	'jen' : 34,
+	'josh' : 42,
+}
+
+print(fav_num)
---
+++ police_glossary.py
@@ -0,0 +1,15 @@
+rank = {
+	'pcso' : 'police comunity support officers',
+	'pc' : 'police counstable',
+	'dc' : 'detective counstable',
+	'ds' : 'dective sergant',
+	'di' : 'dective inspecter',
+	'dmi' : 'digital media invesigator',
+}
+
+print("A PCSO is a " + rank['pcso'].title() + ".")
+print("A PC is a " + rank['pc'].title() + ".")
+print("A DC is a " + rank['dc'].title() + ".")
+print("A DS is a " + rank['ds'].title() + ".")
+print("A DI is a " + rank['di'].title() + ".")
+print("A DMI is a " + rank['dmi'].title() + ".")
