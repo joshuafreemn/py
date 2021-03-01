3ddb0e6f5c235fa29fe128e866448f627e98ae23
Joshua Freeman <70025198+joshuafreemn@users.noreply.github.com>
2021-03-01 21:01:23 +0000

    More Loops

--- favourite_languages.py
+++ favourite_languages.py
@@ -10,3 +10,21 @@ print("Sarah's fave language is " + favorite_languages['sarah'].title() + ".\n")
 #loop through dictionary
 for name, language in favorite_languages.items():
 	print(name.title() + "'s favourite language is " + language.title() + ".")
+
+#loop through keys in dictionary
+print("\nNames:")
+for name in favorite_languages.keys():
+	print(name.title())
+
+#using keys() to find a particular person
+if 'erin' not in favorite_languages.keys():
+	print("\nErin, please take our poll!")
+
+#looping through dictionay in order
+for name in sorted(favorite_languages.keys()):
+	print(name.title() + ", thank you for taking the poll!")
+
+#Looping through values
+print("\nThe follwoing languages have neem mentioned:")
+for language in favorite_languages.values():
+	print("\t" + language.title())
