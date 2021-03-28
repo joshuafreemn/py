543105e2669fa0803cfec0473942f7e6876b6f67
Joshua Freeman (Pop OS) <joshuafreemn@gmail.com>
2021-03-28 14:48:28 +0100

    python script prints maeeage based on party size

---
+++ restaurant_seating.py
@@ -0,0 +1,7 @@
+party = input("How many are in your party? ")
+party = int(party)
+
+if party >= 8:
+	print("Please head to the lounge while we get your table ready.")
+else: 
+	print("Thankyou for dining with us this evening, your table is right this way.")
