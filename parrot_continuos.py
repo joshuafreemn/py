78ceb6e1b12cf81fe8a971598e48332dc0debae7
Joshua Freeman (Pop OS) <joshuafreemn@gmail.com>
2021-03-28 15:34:47 +0100

    adding a flag to parrot file

--- parrot_continuos.py
+++ parrot_continuos.py
@@ -1,10 +1,13 @@
 prompt = "\nTell me something, and i will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
 
-message = ""
-while message != 'quit':
-	message = input(prompt)
-	print("\n" + message)
 
+active = True 
+while active:
+	message = input(prompt)
+	
 	if message != 'quit':
+		print(message)
+
+	else:
 		print(message)
